42df0987b761d75d830268b8c41a392d386cef1a
dreamCode <np03a170120@heraldcollege.edu.np>
2022-02-13 17:12:33 +0545

    fixed line in the title sesction

--- index.php
+++ index.php
@@ -1545,10 +1545,7 @@
 
 
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-    Launch demo modal
-</button>
+
 
 
 
